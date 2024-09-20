69541252b64ce7a928a170f068efeab5a20ee15f
AnfengXu136 <anfengxu136@gmail.com>
2024-09-19 18:38:47 -0700

    added majority filter

--- whisper-modeling/scripts/convert_output.py
+++ whisper-modeling/scripts/convert_output.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def get_timestamps(labels):
     """
     Input: list of labels at each 20ms frame
@@ -32,3 +34,17 @@ def get_timestamps(labels):
             idx += 1
             time += 0.02
     return child, adult, overlap
+
+def majority_filter(pred):
+    """
+    majority filter for smoothing the prediction, window size is 3
+    """
+    new_pred = [0] * len(pred)
+    for i in range(1, len(pred)-1):
+        counter = [0, 0, 0, 0]
+        for j in range(-1, 2):
+            counter[pred[i+j]] += 1
+        new_pred[i] = np.argmax(counter)
+    new_pred[0] = pred[0]
+    new_pred[-1] = pred[-1]
+    return np.array(new_pred)
